9f240f066e7d0fdec83ce7232436eba00f85799b
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-18 13:22:05 +0200

    implement WebSocket server using Fleck

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fleck;
+using System.Collections.Concurrent;
+
+var allSockets = new ConcurrentBag<IWebSocketConnection>();
+
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+server.Start(socket =>
+{
+    socket.OnOpen = () =>
+    {
+        Console.WriteLine("Connected: " + socket.ConnectionInfo.ClientIpAddress);
+        allSockets.Add(socket);
+    };
+
+    socket.OnClose = () =>
+    {
+        Console.WriteLine("Disconnected: " + socket.ConnectionInfo.ClientIpAddress);
+    };
+
+    socket.OnMessage = message =>
+    {
+        Console.WriteLine("Message: " + message);
+        foreach (var s in allSockets)
+        {
+            s.Send(message);
+        }
+    };
+});
+
+Console.WriteLine("WebSocket server started at ws://localhost:8181");
+Console.ReadLine();
